6357a0ed72e071592d3a4afed44677b29a9cd800
Luca Sciascia <luca.sciascia11@gmail.com>
2019-08-11 15:09:35 +0200

    Included var to write pokemon names

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 
 var repository = [
-  {name: "Bulbasaur", height: 0.7, types: ["grass","poison"]},
-  {name: "Charizard", height: 1.7, types: ["fire","flying"]},
-  {name: "Wartortle", height: 1, types: ["water"]}  
+  {name: "Bulbasaur", height: 0.7, types: ["grass","poison"], i: 1},
+  {name: "Charizard", height: 1.7, types: ["fire","flying"], i: 2},
+  {name: "Wartortle", height: 1, types: ["water"], i: 3}
 ];
+
+for (var i = 1; i <= 3; i++) {
+  document.write(name + '(Height:' + Height + ')');
+}
