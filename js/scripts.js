78a4c29581b283f3fb91b0c97c537144581949ce
Luca Sciascia <luca.sciascia11@gmail.com>
2019-08-11 14:45:49 +0200

    Pokemon Repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
 
+var repository = [
+  {name: "Bulbasaur", height: 0.7, types: ["grass","poison"]},
+  {name: "Charizard", height: 1.7, types: ["fire","flying"]},
+  {name: "Wartortle", height: 1, types: ["water"]}  
+];
