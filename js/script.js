9c134683e79c87c48f5f8473d10ade01ad340149
Luca Sciascia <luca.sciascia11@gmail.com>
2019-08-26 09:12:47 +0200

    For is changed to forEach

--- js/script.js
+++ js/script.js
@@ -5,17 +5,6 @@ var repository = [
 { name: "Wartortle", height: 1, types: ['water'], i: 3 }
 ];
 
-for (var i = 0; i < repository.length; i++) {
-
-  if (repository[i].height > 1.1) {
-    document.write (repository[i].name + ' (height:' + repository[i].height + ')' + ' wow, that\'s big' + "<br>")
-  }
-  else {
-    document.write(repository[i].name + ' (height:' + repository[i].height + ')' + "<br>");
-  }
-
-
-
-
-
-}
+repository.forEach(function(arrayitem) {
+  document.write('<p>' + arrayitem.name + ' ' + arrayitem.height + ' ' + arrayitem.types + '</p>');
+});
